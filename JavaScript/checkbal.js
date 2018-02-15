b89d6f26f5bedb45e69d93816770e948077cf696
Hector Arambula <liquid2280@users.noreply.github.com>
2018-02-14 22:18:41 -0700

    Add files via upload

---
+++ JavaScript/checkbal.js
@@ -0,0 +1,21 @@
+/*
+ * Programming Quiz - Checking Your Balance (3-5)
+ */
+
+// change the values of `balance`, `checkBalance`, and `isActive` to test your code
+var balance = -10.00;
+var checkBalance = false;
+var isActive = true;
+
+// your code goes here
+if (isActive === true && checkBalance === true && balance > 0){
+    console.log("Your balance is $"+balance.toFixed(2)+".");
+} else if (isActive === false && checkBalance === true && balance >= 0){
+    console.log("Your account is no longer active.");
+} else if (isActive === true && checkBalance === true && balance === 0){
+    console.log("Your account is empty.");
+} else if (isActive === true && checkBalance === true && balance < 0){
+    console.log("Your balance is negative. Please contact bank.");
+} else if (checkBalance === false){
+    console.log("Thank you. Have a nice day!");
+}
---
+++ JavaScript/tiedup.js
@@ -0,0 +1,2 @@
+var joke = "Why couldn't the shoes go out and play?\nThey were all \"tied\" up!";
+console.log(joke);
