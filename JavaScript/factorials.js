30c571b937d9d0e4c8a123304c898840ca8d4f8d
Hector Arambula <liquid2280@users.noreply.github.com>
2018-02-17 20:35:07 -0700

    Add files via upload
    loops

---
+++ JavaScript/factorials.js
@@ -0,0 +1,9 @@
+/*
+ * Programming Quiz: Factorials (4-7)
+ */
+
+// your code goes here
+var solution = 12;
+for (var i = solution - 1; i > 0; i--) {
+  solution *= i;
+} console.log(solution);
---
+++ JavaScript/fixerror.js
@@ -0,0 +1,12 @@
+/*
+ * Programming Quiz: Fix the Error 1 (4-5)
+ */
+
+// fix the for loop
+for (var x = 5; x < 10; x++) {
+  console.log(x);
+}
+
+for (var k = 0; k < 200; k++) {
+  console.log(k);
+}
