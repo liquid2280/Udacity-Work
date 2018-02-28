f868926acf17b591abc7db520d41978acc8fcbcb
Hector Arambula <liquid2280@users.noreply.github.com>
2018-02-27 19:33:39 -0700

    arrays1
    arrays1

---
+++ crew.js
@@ -0,0 +1,10 @@
+/*
+ * Programming Quiz: Building the Crew (6-2)
+ */
+var crew = [[captain] = "Mal", [second] = "Zoe", [pilot] = "Wash", [companion] = "Inara",
+[mercenary] = "Jayne", [mechanic] = "Kaylee"];
+
+
+// your code goes here
+
+console.log(crew);
---
+++ laughanon.js
@@ -0,0 +1,9 @@
+var laugh = /* finish the function expression */function(num) {
+  var laughs = "ha";
+  for (var i = 1; i < num; i++) {
+    laughs += "ha";
+  }
+  return laughs += "!";
+};
+
+console.log(laugh(10));
