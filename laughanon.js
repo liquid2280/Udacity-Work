01cab2b2fec5d698335cdb1fff9f48b07e44bf72
Hector Arambula <liquid2280@users.noreply.github.com>
2018-02-27 19:34:20 -0700

    Delete laughanon.js

--- laughanon.js
+++
@@ -1,9 +0,0 @@
-var laugh = /* finish the function expression */function(num) {
-  var laughs = "ha";
-  for (var i = 1; i < num; i++) {
-    laughs += "ha";
-  }
-  return laughs += "!";
-};
-
-console.log(laugh(10));
